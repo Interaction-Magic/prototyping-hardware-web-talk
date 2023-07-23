2bb1326c8153c8540a6f3a9d2109c84868d51672
George Cave <cave@hey.com>
2023-07-23 14:51:20 +0100

    Update mischief programs

--- 01_Keyboard/mischief-delete-everything/mischief-delete-everything.ino
+++ 01_Keyboard/mischief-delete-everything/mischief-delete-everything.ino
@@ -1,6 +1,13 @@
 #include <Keyboard.h>
 
 void setup(){
+	
+	// ⚠ Safety stop - program will not begin when pin 12 connected to GND
+	pinMode(12, INPUT_PULLUP);
+	while(digitalRead(12)==LOW){
+		delay(500);
+	}
+	
 	Keyboard.begin();
 
 	// Select everything
--- 01_Keyboard/mischief-move-mouse/mischief-move-mouse.ino
+++ 01_Keyboard/mischief-move-mouse/mischief-move-mouse.ino
@@ -1,19 +1,21 @@
-#include <Keyboard.h>
+#include <Mouse.h>
 
 void setup(){
+	Mouse.begin();
+}
 
+void loop(){
+	
 	// ⚠ Safety stop - program will not begin when pin 12 connected to GND
 	pinMode(12, INPUT_PULLUP);
 	while(digitalRead(12)==LOW){
 		delay(500);
 	}
 	
-	Keyboard.begin();
-}
-
-void loop(){
-	Keyboard.press(KEY_LEFT_ALT);
-	Keyboard.press(KEY_F4);
-	Keyboard.releaseAll();
-	delay(30*1000);
+	Mouse.move(
+		random(-5, 5),
+		random(-5, 5),
+		0
+	);
+	delay(5*1000);
 }
